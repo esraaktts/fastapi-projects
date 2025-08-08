1affb790ed4e8b3538bf60bfb5c7849005870df9
esraaktts <f231213043@ktun.edu.tr>
2025-08-08 09:37:11 +0300

    Created redis_client.py

---
+++ redis_client.py
@@ -0,0 +1,9 @@
+import redis
+
+_redis_client = None
+
+def get_redis_client():
+    global _redis_client
+    if _redis_client is None:
+        _redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    return _redis_client
